dc3af74cc64c44c51950b84af1586fcd584d8dc4
qq1327300698 <1327300698@qq.com>
2018-04-14 11:19:48 +0800

    xml

---
+++ js/shiyan.js
@@ -0,0 +1,14 @@
+if (window.XMLHttpRequest)
+ {// code for IE7+, Firefox, Chrome, Opera, Safari
+ xmlhttp=new XMLHttpRequest();
+ }
+ else
+ {// code for IE6, IE5
+ xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+ }
+ xmlhttp.open("GET","gdg.xml",false);
+ xmlhttp.send();
+ xmlDoc=xmlhttp.responseXML;
+
+ document.getElementById("shiyan").innerHTML=
+ xmlDoc.getElementsByTagName("herf")[0].nodeValue;
